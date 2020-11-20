2e96bcf409dd6706541a3fd541a52a189801d054
RebeccaMariahDEV <71558611+RebeccaMariahDEV@users.noreply.github.com>
2020-11-20 16:07:37 -0300

    mudanças no front
    alterações de api

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,31 +1,61 @@
 import React, {useState} from 'react';
+import Api from '../services/api';
 
 import Input from '../components/Input';
 
 import LogoMagalu from '../assets/images/magalu_logo.png';
 
 function Login() {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [usernameLogin, setUserNameLogin] = useState('');
+  const [passwordLogin, setPasswordLogin] = useState('');
+  const [usernameRegister, setUserNameRegister] = useState('');
+  const [passwordRegister, setPasswordRegister] = useState('');
 
-  const handleSubmit = e => {
+  const handleLogin = e => {
     e.preventDefault();
+
+    const formData = new FormData();
+    formData.set('username', usernameLogin);
+    formData.set('password', passwordLogin);
+
+    Api.post('auth/login/', formData)
+        .then((res)=>{
+          console.log('deu bom', res);
+        })
+        .catch((error)=>{
+          console.log('deu erro', error);
+        })
+  }
+  const handleRegister = e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.set('username', usernameRegister);
+    formData.set('password', passwordRegister);
+
+    Api.post('users/register/', formData)
+        .then((res)=>{
+          console.log('deu bom', res);
+        })
+        .catch((error)=>{
+          console.log('deu erro', error);
+        })
   }
 
   return (
     <div className="container-fluid">
-      <main className="login row d-flex align-items-center justify-content-center flex-column">
+      <main className="login row d-flex align-items-center justify-content-center flex-column p-3">
         <img src={LogoMagalu} alt=""/>
-        <div class="content-login col-md-7">
+        <div className="content-login col-md-7">
           <div className="row d-flex align-items-center justify-content-center">
             <div className='card-login col-6 d-flex align-items-center justify-content-center flex-column'>
             <h3>Cadastre-se Agora!</h3>
               <p>Registra-se agora para vender no Magalu.</p> <hr/>
               <form>
-                <Input value={username} onChange={e => setUserName(e.target.value)} type="text" label="Nome de Usuário" id="input-username" placeholder='Digite um usuário' />
+                <Input value={usernameRegister} onChange={e => setUserNameRegister(e.target.value)} type="text" label="Nome de Usuário" id="register-input-username" placeholder='Digite um usuário' />
 
-                <Input value={password} onChange={e => setPassword(e.target.value)} type="password" label="Senha" id="input-email" placeholder='Digite senha' />
-                <button className="btn btn-success my-2 my-sm-0" onClick={handleSubmit}>
+                <Input value={passwordRegister} onChange={e => setPasswordRegister(e.target.value)} type="password" label="Senha" id="register-input-email" placeholder='Digite senha' />
+                <button className="btn btn-success my-2 my-sm-0" onClick={handleRegister}>
                   Cadastrar
                 </button>
               </form>
@@ -36,10 +66,10 @@ function Login() {
               <h3>Já tem cadastro? Entre</h3>
               <p>Entrar no Você no Magalu :)</p> <hr/>
               <form>
-                <Input value={username} onChange={e => setUserName(e.target.value)} type="text" label="Nome de Usuário" id="input-username" placeholder='Digite um usuário' />
+                <Input value={usernameLogin} onChange={e => setUserNameLogin(e.target.value)} type="text" label="Nome de Usuário" id="login-input-username" placeholder='Digite um usuário' />
 
-                <Input value={password} onChange={e => setPassword(e.target.value)} type="password" label="Senha" id="input-email" placeholder='Digite senha' />
-                <button className="btn btn-success my-2 my-sm-0" onClick={handleSubmit}>
+                <Input value={passwordLogin} onChange={e => setPasswordLogin(e.target.value)} type="password" label="Senha" id="login-input-email" placeholder='Digite senha' />
+                <button className="btn btn-success my-2 my-sm-0" onClick={handleLogin}>
                   Entrar
                 </button>
               </form>
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,39 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://localhost:8000'
-})
+class Api {
 
-export default api;
+    // configuration
+    endpoint = 'http://localhost:8000';
+    headers = {
+        'Accept':'application/json',
+        'Authorization': null
+    }
+
+    setAuth = token => {
+        this.headers.Authorization = `Bearer ${token}`
+    }
+    post = (action, parameters) => {
+        const promise = axios({
+            method: 'POST',
+            url: this.endpoint + '/' + action,
+            data: parameters,
+            headers: this.headers
+        });
+
+        return promise;
+    }
+
+    get = (action, parameters) => {
+        const promise = axios({
+            method: 'GET',
+            url: this.endpoint + '/' + action,
+            data: parameters,
+            headers: this.headers
+        });
+
+        return promise;
+    }
+
+}
+
+export default new Api();
